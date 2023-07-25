eaf5539de3de48584bd13a348c28b3be699eaaa9
Yasir Musa <baydreafrica@gmail.com>
2023-07-25 13:30:21 -0700

    UNIX 0.1: function that executes user's command

---
+++ cmd_executor.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * execute_command - creates and executes a child process
+ * using 'fork' and 'execve'.
+ * @args: arguments to be executed. 
+ */
+void execute_command(char **args)
+{
+	pid_t c_pro;
+	int status;
+
+	c_pro = fork();
+	if (c_pro == -1)
+	{
+		perror("fork");
+		exit(EXIT_FAILURE);
+	}
+	else if (c_pro == 0)
+	{
+		execvp(args[0], args);
+		perror(args[0]);
+		exit(EXIT_FAILURE);
+	}
+	else
+	{
+		waitpid(c_pro, &status, 0);
+	}
+}
