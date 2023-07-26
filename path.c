249dd9992f901de2c75166952a9b41f67d51d4d4
JessicaChideraOgu <ogujessica3@gmail.com>
2023-07-26 16:11:40 +0100

    Updated path.c file

--- path.c
+++ path.c
@@ -10,51 +10,52 @@
  */
 int find_file(char *path)
 {
-        struct stat buff;
-        return ((stat(path, &buff) == 0));
+	struct stat buff;
+
+	return ((stat(path, &buff) == 0));
 }
 
 /**
  * witch -
  * filename:
  * path:
- */ 
+ */
 char *witch(char *filename, char *path)
 {
-        char *path_cpy;
-        char *tokens[1024];
-        char *pathname;
-        int i;
+	char *path_cpy;
+	char *tokens[1024];
+	char *pathname;
+	int i;
 
 	if (path == NULL)
 	{
-          return (NULL);
-        }
-        path_cpy = strdup(path);
+		return (NULL);
+	}
+	path_cpy = strdup(path);
 
-        /* Tokenize path */
-        i = 0;
+	/* Tokenize path */
+	i = 0;
 
-        tokens[i] = strtok(path_cpy, ":");
+	tokens[i] = strtok(path_cpy, ":");
 
-        while (tokens[i] != NULL)
-        {
-                pathname = malloc(strlen(tokens[i]) + strlen(filename) + 2);
-                strcpy(pathname, tokens[i]);
+	while (tokens[i] != NULL)
+	{
+		pathname = malloc(strlen(tokens[i]) + strlen(filename) + 2);
+		strcpy(pathname, tokens[i]);
 		strcat(pathname, "/");
 		strcat(pathname, filename);
 
-                if (find_file(pathname))
-                {
-                        free(path_cpy);
-                        return(pathname);
-                }
+	if (find_file(pathname))
+	{
+		free(path_cpy);
+		return (pathname);
+	}
 
-                free(pathname);
-                i++;
-                tokens[i] = strtok(NULL, ":");
-        }
+		free(pathname);
+		i++;
+		tokens[i] = strtok(NULL, ":");
+	}
 
-        free(path_cpy);
-        return (NULL);
+	free(path_cpy);
+	return (NULL);
 }
