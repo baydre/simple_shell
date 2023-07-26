23d5b722ae68301c5e55612220b561bd8cf1c3d3
Yasir Musa <baydreafrica@gmail.com>
2023-07-26 07:22:10 -0700

    UNIX: Updated main function with v0.2 - v1.0

--- shell.c
+++ shell.c
@@ -6,31 +6,37 @@
  */
 int main(void)
 {
-	char *command;
+	char *command, *str_path, *fetch_get = getenv("PATH");
 	char **tokens;
-
+	
 	while (1)
 	{
 		if (isatty(STDIN_FILENO))
-		{
 			prompt_display();
-		}
+		
 		command = cmd_read();
 
+		tokens = tokenise(command);
 		if (tokens == NULL)
 		{
-			free(command);
-			continue;
+                        free(command);
+                        continue;
 		}
-
-		tokens = tokenize(command);
+		if (isExit(tokens))
+                        break;
+                if (isEnv(tokens))
+                        continue;
 		if (access(tokens[0], X_OK) == -1)
 		{
-			write_error_message(tokens[0]);
+			str_path = witch(tokens[0], fetch_get);
+			if (str_path != NULL)
+				execute_command(str_path, tokens);
+			else
+				write_error_message(tokens[0]);
 		}
 		else
 		{
-			execute_command(tokens);
+			execute_command(tokens[0], tokens);
 		}
 		free(command);
 		free(tokens);
