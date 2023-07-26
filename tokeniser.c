9991b56732cbfc486aff69d747202ef469c3cceb
Yasir Musa <baydreafrica@gmail.com>
2023-07-26 07:37:21 -0700

    UNIX 0.2: function that tokenise shell PATH

---
+++ tokeniser.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * tokenise - tokenises the array of tokens
+ * Return: token count.
+ */
+char **tokenise(char *command)
+{
+	char **tokens = malloc(max_len_cmd * sizeof(char *));
+	char *token = strtok(command, " \t\n");
+	int count = 0;
+
+	if (tokens == NULL)
+	{
+		perror("malloc");
+		exit(EXIT_FAILURE);
+	}
+	while (token != NULL)
+	{
+		tokens[count] = strdup(token);
+		if (tokens[count] == NULL)
+		{
+			perror("strdup");
+			exit(EXIT_FAILURE);
+		}
+		count++;
+		token = strtok(NULL, " \t\n");
+	}
+	tokens[count] = NULL;
+	return (tokens);
+}
