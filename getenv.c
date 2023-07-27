f2dae3c4eb7849ce9e6bb0d6c424bc44a7d32739
JessicaChideraOgu <ogujessica3@gmail.com>
2023-07-28 00:39:03 +0100

    Fixed betty style checks

--- getenv.c
+++ getenv.c
@@ -1,16 +1,20 @@
 #include "main.h"
-
-char* _getenv(const char* name)
+/**
+ * _getenv - short descp
+ * @name: the name of the environment
+ * Return: Returns NULL.
+ */
+char *_getenv(const char *name)
 {
 	int i;
 	size_t namelen = _strlen(name);
-	
+
 	for (i = 0; environ[i] != NULL; i++)
 	{
 		if (strncmp(name, environ[i], namelen) == 0 && environ[i][namelen] == '=')
 		{
-			return &environ[i][namelen + 1];
+			return (&environ[i][namelen + 1]);
 		}
 	}
-	return NULL;
+	return (NULL);
 }
