70705550aa7c083c7e1180de7d11b6c83783c11a
Yasir Musa <baydreafrica@gmail.com>
2023-07-27 15:04:59 -0700

    UNIX: main function file

---
+++ shell.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+/**
+ * main - entry point.
+ * Return: zero.
+ */
+int main(int argc __attribute__((unused)), char **argv)
+{
+	char *line = NULL;
+	size_t length = 0;
+	char *tokens[1024];
+	int count, output;
+	char *fullpath;
+	int exit_status = 0;
+
+	while (1)
+	{
+		output = prompt_handler(&length, &line);
+		if (output == 1)
+			break;
+		count = 0;
+		tokens[count] = strtok(line, " \t\n");
+		while (tokens[count] != NULL)
+		{
+			count++;
+			tokens[count] = strtok(NULL, " \t\n");
+		}
+		tokens[count] = NULL;
+		fullpath = tokens[0];
+		output = builtin_checker(count, tokens, &exit_status);
+		if (output == 1)
+			break;
+		else if (output == 2)
+			continue;
+		output = error_checker(argv, tokens, &fullpath, &exit_status);
+		if (output == 1)
+			break;
+		else if (output == 2)
+			continue;
+		output = command_execute(&exit_status, fullpath, tokens);
+		if (output == 1)
+			break;
+		else if (output == 2)
+			continue;
+	}
+	free(line);
+	return (exit_status);
+}
